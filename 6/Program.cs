189ff1890273606af43f1a5df163df130231546e
ADM <ADM@A320M-H-CF>
2022-01-28 02:12:31 +0300

    Добавьте файлы проекта.

---
+++ 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string String1 = Console.ReadLine();
+            string String2 = "";
+            String1 = String1.Replace(" ", ""); ;
+            Console.WriteLine(String1);
+            int result;
+            for (int i = 0; i < String1.Length; i++)
+            {
+                String2 += String1[String1.Length - i - 1];
+            }
+            Console.WriteLine(String2);
+            result = String.Compare(String1, String2,true);
+            if (result == 0)
+                Console.WriteLine("палиндром");
+            else
+                Console.WriteLine("не палиндром");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string startstring = Console.ReadLine();
+            string[] stringArray = startstring.Split();
+            int [] vs = new int[stringArray.Length];
+            int max = -1;
+            int index = -1;
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                vs[i] = stringArray[i].Length;
+            }
+            for (int i = 0; i < vs.Length; i++)
+                if (max <= vs[i])
+                {
+                    max = vs[i];
+                    index = i;
+                }
+            Console.WriteLine(stringArray[index]);   
+            Console.ReadKey();
+        }
+    }
+}
